9e2447954885d9840b0405360ac0840e07c3c973
Your Name <you@example.com>
2022-09-07 08:48:15 +0800

    init

--- ping.py
+++ ping.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-
+#!/usr/bin/env python3
 import requests, time
 import logging, sys
 from halo import Halo
@@ -11,11 +9,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 #################
 ### scheduler ###
 #################
+current_api = 15
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=3)
 def main():
-  current_api = 10
-
   for i in range(current_api+1):
     try:
       LOADING = None
@@ -30,5 +27,20 @@ def main():
       print(f"{i} {HOST} Finished with {int(finished_time)} /seconds delay..")
     except Exception as e:
       print(e);continue
+
+@sched.scheduled_job('interval', minutes=30)
+def restart():
+  tokens=open('tokens.txt').read().splitlines()
+  for i in range(current_api+1):
+    token = tokens[i]
+    baseurl = f'https://api.heroku.com/apps/ping-00{i}/dynos'
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/vnd.heroku+json; version=3',
+      'Authorization': token
+    }
+
+    response = requests.delete(baseurl, headers=headers)
+    i+=1
       
 sched.start()
